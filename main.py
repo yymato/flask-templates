1439849cc777f3e76eb0e62189d71668e7ea2943
yymato <070913a@gmail.com>
2025-03-23 09:52:37 +0300

    Задача Список профессий

--- main.py
+++ main.py
@@ -16,5 +16,14 @@ def index(title):
 def training_flight(param):
     return render_template('training_in_flight.html', param=param)
 
+@app.route('/list_prof/<lst>')
+def list_prof(lst):
+    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
+    return render_template('list_of_professions.html', list=lst, list_prof=professions)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
